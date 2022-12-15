051aa5d1e3651f9dd5af17a276ee998b54483171
Nahum Shalman <nahamu@gmail.com>
2022-12-15 01:52:56 +0000

    sunos: Build and ship releases on GitHub

--- build.sh
+++ build.sh
@@ -3,43 +3,29 @@
 set -o xtrace
 set -o errexit
 
-
 export GOOS=${1-illumos}
 
 # This prevents illumos libc from leaking into Solaris binaries when built on illumos
 export CGO_ENABLED=0
 
-for cmd in ./cmd/tailscale ./cmd/tailscaled; do
-	bash -x ./build_dist.sh "${cmd}"
+fix_osabi () {
 	if [[ $(uname -s) == SunOS ]]; then
 		/usr/bin/elfedit \
 			-e "ehdr:ei_osabi ELFOSABI_SOLARIS" \
 			-e "ehdr:ei_abiversion EAV_SUNW_CURRENT" \
-			"$(basename $cmd)"
+			"${1?}"
 	else
-		elfedit --output-osabi "Solaris" --output-abiversion "1" "$(basename $cmd)"
+		elfedit --output-osabi "Solaris" --output-abiversion "1" "${1?}"
 	fi
+}
+
+for GOOS in illumos solaris; do
+	export GOOS
+	bash -x ./build_dist.sh --box ./cmd/tailscaled
+	fix_osabi tailscaled
+	mv tailscaled{,-${GOOS}}
 done
 
-pkgver=$(cat VERSION.txt)
-pkgdir=${pkgver}-${GOOS}
-
-mkdir ${pkgdir}
-mv tailscale tailscaled ${pkgdir}
-cp cmd/tailscaled/tailscale.xml ${pkgdir}
-cp $0 ${pkgdir}/build.sh
-cd ${pkgdir}
-shasum -a 256 * >sha256sums
-cat >index.html <<EOF
-<html>
-<head><title>${GOOS} build of Tailscale ${pkgver}</title></head>
-${GOOS} build of Tailscale ${pkgver}
-<ul>
-<li><a href="tailscale">tailscale</a></li>
-<li><a href="tailscaled">tailscaled</a></li>
-<li><a href="tailscale.xml">tailscale.xml</a></li>
-<li><a href="sha256sums">sha256sums</a></li>
-<li><a href="build.sh">build.sh</a></li>
-</ul>
-</html>
-EOF
+ln cmd/tailscaled/tailscale.xml .
+shasum -a 256 tailscaled-* tailscale.xml >sha256sums
+rm ./tailscale.xml
